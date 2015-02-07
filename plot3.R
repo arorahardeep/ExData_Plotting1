d90f183f547c4180fcde72f7eae15d2e56b2c51c
arorahardeep <hardeeparora@yahoo.com>
2015-02-07 17:53:44 +0800

    Fixed the code for getting proper legends

--- plot3.R
+++ plot3.R
@@ -4,10 +4,14 @@ plot3 <- function() {
   rm(x)
   xd$Date <- as.Date(xd$Date,format = "%d/%m/%Y")
   xd$Time <- as.POSIXct(paste(xd$Date,xd$Time))
+  
+  ## Opened a png Device directly to work around the Legends clipping issue  
+  png(file="plot3.png", height=480, width=480)
+  
   plot(xd$Sub_metering_1~xd$Time,type="l",ylab="Energy sub metering", xlab="")
   lines(xd$Sub_metering_2~xd$Time,type="l",col="red")
   lines(xd$Sub_metering_3~xd$Time,type="l",col="blue")
-  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,lwd=2,cex=0.6)
-  dev.copy(png,file="plot3.png", height=480, width=480)
+  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,lwd=2,cex=0.8)
+  #dev.copy(png,file="plot3.png", height=480, width=480)
   dev.off()
 }
--- plot4.R
+++ plot4.R
@@ -4,6 +4,9 @@ plot4 <- function() {
   rm(x)
   xd$Date <- as.Date(xd$Date,format = "%d/%m/%Y")
   xd$Time <- as.POSIXct(paste(xd$Date,xd$Time))
+  
+  ## Opened a png Device directly to work around the Legends clipping issue
+  png(file="plot4.png", height=480, width=480)
   par(mfrow=c(2,2))
   
   plot(xd$Global_active_power~xd$Time,type="l",ylab="Global Active Power (kilowatts)", xlab="")
@@ -14,10 +17,10 @@ plot4 <- function() {
   plot(xd$Sub_metering_1~xd$Time,type="l",ylab="Energy sub metering", xlab="")
   points(xd$Sub_metering_2~xd$Time,type="l",col="red")
   points(xd$Sub_metering_3~xd$Time,type="l",col="blue")
-  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,lwd=2,cex=0.5,bty="n")
+  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,lwd=2,bty="n")
   
   plot(xd$Global_reactive_power~xd$Time,type="l",ylab="Global_reactive_power", xlab="datetime")
   
-  dev.copy(png,file="plot4.png", height=480, width=480)
+  #dev.copy(png,file="plot4.png", height=540, width=540)
   dev.off()
 }
